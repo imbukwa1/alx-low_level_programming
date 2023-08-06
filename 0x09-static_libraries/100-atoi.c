1f40d6b13c56247265191eebe5fdfe416dfb88fc
imbukwa1 <imbukwaaudry@gmail.com>
2023-08-06 11:18:25 -0700

    hello

---
+++ 0x09-static_libraries/100-atoi.c
@@ -0,0 +1,7 @@
+#include "main.h"
+/**
+ * _atoi - convert a string into an integer.
+ * @s: the string to use.
+ * Return: integer.
+ */
+
---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen - return the lenth of a string
+ * @s: string
+ * return: length
+ */
+int _strlen(char *s)
+{
+	int longi = 0;
+
+	while (*s != '\0')
+	{
+		longi++;
+		s++;
+	}
+
+	return (longi);
+}
